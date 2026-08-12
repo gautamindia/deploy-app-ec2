f9a23729c5e61c1b7360ec731ccb81423762d923
gautam saini <you@example.com>
2026-08-12 08:20:25 +0000

    3

--- .github/workflows/userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dnf update -y
-dnf install -y nginx
-
-systemctl enable nginx
-systemctl start nginx
-
-echo "<h1>Hello from GitHub Actions EC2</h1>" > /usr/share/nginx/html/index.html
